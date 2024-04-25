fc7606acffb4d5dacdcf17d3bf20dba005c8efa5
itmstore <mishazotov1234@gmail.com>
2024-04-25 15:24:19 +0200

    Update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 def main():
     st.title("Hello, Streamlit!")
     st.write("This is a simple Streamlit app.")
+    st.write("This is the modified title")
 
 if __name__ == "__main__":
     main()
